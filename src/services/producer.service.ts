3820a57ffe7a04c3586affc3282d66ede05b1e61
Arsen Arakelian <arsen.arakelian@chisw.com>
2021-02-26 22:14:38 +0200

    write produce with async/await syntax

--- src/services/producer.service.ts
+++ src/services/producer.service.ts
@@ -1,71 +1,36 @@
-import * as amqp from 'amqplib/callback_api';
+import * as amqp from 'amqplib';
 import * as express from 'express';
 
 class MessageListner {
-  // try writing with callback syntax
-
   async produce(file: any): Promise<void> {
-    await amqp.connect(process.env.AMQP_URL, (err: any, conn: any) => {
-      if (err) {
-        console.error('We have a problem with connection...', err);
-      }
+    try {
+      const conn = await amqp.connect(`${process.env.AMQP_URL}`);
       console.log('[x] Connection created...');
 
-      conn.createChannel((err: any, ch: any) => {
-        if (err) {
-          console.error('We have a problem with creating channel...', err);
-        }
-        console.log('[x] Channel created...');
+      const ch = await conn.createChannel();
+      console.log('[x] Channel created...');
 
-        const queue = 'data_queue';
-        const image = file;
+      const queue = 'data_queue';
+      const msg = file;
 
-        ch.assertQueue(queue, {
-          durable: false,
-        });
-
-        ch.sendToQueue(queue, Buffer.from(JSON.stringify(image)), {
-          persistent: true,
-        });
-        console.log('[x] Sent', JSON.stringify(image));
-
-        setTimeout(() => {
-          conn.close();
-          console.log('[x] Closing rabbitmq channel');
-          process.exit(0);
-        }, 500);
+      await ch.assertQueue(queue, {
+        durable: false,
       });
-    });
 
-    // try writing with async/await syntax
+      await ch.sendToQueue(queue, Buffer.from(JSON.stringify(msg)), {
+        persistent: true,
+      });
 
-    //   try {
-    //   const conn = await amqp.connect('amqp://localhost:5672', conn: any);
-    //   console.log('[x] Connection created...');
+      console.log('[x] Sent', msg);
 
-    //   const ch = await conn.createChannel()
-    //   console.log('[x] Channel created...');
-
-    //   const queue = 'data_queue';
-    //   const data = file.file_path;
-
-    //   ch.assertQueue(queue, {
-    //     durable: false,
-    //   });
-
-    //   ch.sendToQueue(queue, Buffer.from(JSON.stringify(data)), {
-    //     persistent: true,
-    //   });
-
-    //   console.log('[x] Sent', data);
-
-    //   setTimeout(() => {
-    //     conn.close();
-    //     console.log('[x] Closing rabbitmq channel');
-    //     process.exit(0);
-    //   }, 500);
-    // }
+      await ch.close();
+      await conn.close();
+      console.log('[x] Closing rabbitmq channel');
+      process.exit(0);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
-export default new MessageBroker();
+export default new MessageListner();
